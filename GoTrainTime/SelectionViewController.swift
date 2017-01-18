1b5b150f87618bd4acc336e0e82c25c739b617e9
JackY <wo131485508@qq.com>
2017-01-17 21:46:27 -0500

    enterButton Set

--- GoTrainTime/SelectionViewController.swift
+++ GoTrainTime/SelectionViewController.swift
@@ -10,43 +10,91 @@ import UIKit
 
 class SelectionViewController: UIViewController {
     
-    var fromField: UITextField!
+    var inputField: UITextField!
     var verticalConForFromField: NSLayoutConstraint!
+    var enterButton: UIButton!
+    var inputStage: InputStage!
+    
+    enum InputStage{
+        case fromStage
+        case toStage
+        case pickTimeStage
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addFromField()
+        inputStage = .fromStage
+        addInputField()
+        addEnterButton()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         verticalConForFromField.constant = 0
-        UIView.animate(withDuration: 2.0, animations: {
+        UIView.animate(withDuration: 1.0, animations: {
             self.view.layoutIfNeeded()
         })
     }
     
-    func addFromField(){
-        fromField = UITextField()
-        fromField.placeholder = "From"
-        fromField.backgroundColor = UIColor.gray
-        fromField.textColor = UIColor.orange
-        let horizontalCon = NSLayoutConstraint(item: fromField, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
-        verticalConForFromField = NSLayoutConstraint(item: fromField, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: -500)
-        let widthCon = NSLayoutConstraint(item: fromField, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
-        let heightCon = NSLayoutConstraint(item: fromField, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 30)
+    func addInputField(){
+        inputField = UITextField()
+        inputField.placeholder = "From"
+        inputField.backgroundColor = UIColor.green
+        inputField.textColor = UIColor.orange
+        let horizontalCon = NSLayoutConstraint(item: inputField, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        verticalConForFromField = NSLayoutConstraint(item: inputField, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: -500)
+        let widthCon = NSLayoutConstraint(item: inputField, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 150)
+        let heightCon = NSLayoutConstraint(item: inputField, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 30)
         
-        fromField.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(fromField)
+        inputField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(inputField)
         view.addConstraints([horizontalCon, verticalConForFromField, widthCon, heightCon])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addEnterButton(){
+        enterButton = UIButton()
+        enterButton.setTitle("Enter", for: .normal)
+        enterButton.addTarget(self, action: #selector(hitEnter), for: .touchUpInside)
+        enterButton.backgroundColor = UIColor.red
+        let horizontalCon = NSLayoutConstraint(item: enterButton, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: inputField, attribute: NSLayoutAttribute.right, multiplier: 1, constant: 5)
+        let verticalCon = NSLayoutConstraint(item: enterButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)
+        let widthCon = NSLayoutConstraint(item: enterButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 60)
+        let heightCon = NSLayoutConstraint(item: enterButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 30)
+        
+        enterButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(enterButton)
+        view.addConstraints([horizontalCon, verticalCon, widthCon, heightCon])
     }
-
+    
+    func hitEnter(){
+        if let inputStage = inputStage{
+            switch inputStage {
+            case .fromStage:
+                applyFromStageEnter()
+            case .toStage:
+                applyToStageEnter()
+            case .pickTimeStage:
+                applyPickTimeStageEnter()
+            }
+        }
+    }
+    
+    func applyFromStageEnter(){
+        print(inputField.text!)
+        verticalConForFromField.constant = 600
+        UIView.animate(withDuration: 1.0, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func applyToStageEnter(){
+        
+    }
+    
+    func applyPickTimeStageEnter(){
+        
+    }
 
 }
 
